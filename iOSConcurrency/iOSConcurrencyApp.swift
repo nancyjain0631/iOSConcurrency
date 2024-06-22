50bd4b094936b494261cf4ebf4180f184387347c
Nancy Jain <nancyjain0631@gmail.com>
2024-06-22 21:42:06 +0530

    Initial Commit

---
+++ iOSConcurrency/iOSConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSConcurrencyApp.swift
+//  iOSConcurrency
+//
+//  Created by Nancy Jain on 22/06/24.
+//
+
+import SwiftUI
+
+@main
+struct iOSConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
